4a3bc06f909b8949ba7fe5bb787d8035c4492cfe
juliandunne1234 <julianmorandunne@gmail.com>
2022-12-17 14:18:11 +0000

    completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = (
         'order_number', 'date', 'delivery_cost',
-        'order_total', 'grand_total',
+        'order_total', 'grand_total', 'original_bag', 
+        'stripe_pid',
     )
 
     fields = (
@@ -20,7 +21,8 @@ class OrderAdmin(admin.ModelAdmin):
         'email', 'phone_number', 'country',
         'postcode', 'town_or_city', 'street_address1',
         'street_address2', 'county', 'delivery_cost',
-        'order_total', 'grand_total',
+        'order_total', 'grand_total', 'original_bag', 
+        'stripe_pid',
     )
 
     list_display = (
